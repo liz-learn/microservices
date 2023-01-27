b809396c7e2a6b1e71bf174cafe371a07a646f70
Liz Schley <liz.schley@advance-auto.com>
2023-01-27 16:48:43 -0500

    table created

--- customer/src/main/java/liz/customer/CustomerService.java
+++ customer/src/main/java/liz/customer/CustomerService.java
@@ -2,12 +2,13 @@ package liz.customer;
 
 import org.springframework.stereotype.Service;
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
+        customerRepository.save(customer);
     }
 }
